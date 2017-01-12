b3599fd79ceda39ae575b2c3d28647ed431d3cab
dpfaffenbauer <dominik@lineofcode.at>
2017-01-12 08:51:30 +0100

    change license to MIT

--- src/Message/XmlRequest.php
+++ src/Message/XmlRequest.php
@@ -4,12 +4,12 @@
  *
  * LICENSE
  *
- * This source file is subject to the GNU General Public License version 3 (GPLv3)
- * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
- * files that are distributed with this source code.
+ * This source file is subject to the MIT License
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that are distributed with this source code.
  *
  * @copyright  Copyright (c) 2016 Woche-Pass AG (http://www.w-vision.ch)
- * @license    GNU General Public License version 3 (GPLv3)
+ * @license    MIT License
  */
 
 namespace Omnipay\Datatrans\Message;
